5fe44d36cb529a098ebf3126034f616d9be06686
vorlasit <svorlasit@gmail.com>
2025-01-07 13:23:10 +0700

    custom lod_broad_chat

---
+++ custome/lod_broad_chat/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Invoicing',
+    'version': '1.3',
+    'summary': 'Invoices, Payments, Follow-ups & Bank Synchronization',
+    'sequence': 10,
+    'description': """ """,
+    'category': 'all',
+    'website': 'https://www.odoo.com/app/invoicing',
+    'depends': ['base_setup',  ],
+    'data': [
+         
+    ],
+    'demo': [
+         
+    ],
+    'installable': True,
+    'application': True,
+     
+    'license': 'LGPL-3',
+}
